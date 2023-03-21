85033a9f22a402446208b1bb05d1d5391182c81e
Lsong <song940@gmail.com>
2023-03-21 13:29:03 +0800

    update

--- location.js
+++ location.js
@@ -1,4 +1,36 @@
 
+const { geolocation } = navigator;
+
+
+/**
+ * Geolocation.getCurrentPosition()
+ * @docs https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
+ * @param {*} options 
+ * @returns 
+ */
+export const getCurrentPosition = options => {
+  if (!geolocation) Promise.reject('does not support geolocation');
+  return new Promise((resolve, reject) => {
+    geolocation.getCurrentPosition(resolve, reject, options);
+  });
+};
+
+/**
+ * Geolocation.watchPosition()
+ * @docs https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/watchPosition
+ * @param {*} fn 
+ * @param {*} options 
+ * @returns 
+ */
+export const watchPosition = (fn, options) => {
+  var id;
+  return {
+    // @docs https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/clearWatch
+    stop: geolocation.clearWatch(id),
+    start: () => id = geolocation.watchPosition(fn.bind(null, null), fn, options),
+  }
+};
+
 export const distance = (lat1, lon1, lat2, lon2) => {
   const R = 6371; // Radius of the earth in km
   const c = Math.cos;
@@ -10,7 +42,7 @@ export const distance = (lat1, lon1, lat2, lon2) => {
   return (2 * R) * Math.asin(Math.sqrt(a));
 };
 
-export class GPS extends EventEmitter {
+export class LocationService extends EventEmitter {
   constructor(options) {
     super();
     Object.assign(this, {
@@ -22,14 +54,7 @@ export class GPS extends EventEmitter {
   enable() {
 
   }
-  getCurrentPosition() {
-    const { geolocation } = navigator;
-    if (!geolocation) Promise.reject('denied');
-    return new Promise((resolve, reject) => {
-      geolocation.getCurrentPosition(resolve, reject, this);
-    });
-  }
 }
 
-export const gps = new GPS();
-export const requestLocation = () => gps.getCurrentPosition();
+export const location = new LocationService();
+export const requestLocation = getCurrentPosition;
