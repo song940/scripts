50b6a0d33c7630d50a6c8a756bfc554b12988e2b
Lsong <song940@gmail.com>
2024-04-15 12:17:12 +0800

    update

--- react/index.js
+++ react/index.js
@@ -1,2 +1,16 @@
 export * from "./react.js";
 export * from "./components/index.js";
+
+import { useState, useEffect } from "./react.js";
+
+export function useLocalStorageState(key, defaultValue) {
+  const [state, setState] = useState(defaultValue);
+  const storedValue = localStorage.getItem(key);
+  useEffect(() => {
+    setState(storedValue !== null ? JSON.parse(storedValue) : defaultValue);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+  return [state, setState];
+}
